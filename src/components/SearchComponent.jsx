46f913bdc9c71fd88e2caffb93765a722512dcfa
jaimesanchezgalvisacct <jaime.sanchez@acctglobal>
2023-03-09 20:46:40 +0100

    feat: add filter

--- src/components/SearchComponent.jsx
+++ src/components/SearchComponent.jsx
@@ -1,44 +1,49 @@
 import { Container, Grid, Input, Loading, Row, Text } from "@nextui-org/react";
+import { useForm } from "../hooks/useForm";
+import { useQuery } from "react-query";
+import { getPodCast } from "../api/getPodCast";
+import PodCastCard from "./PodCastCard";
 
 export const SearchComponent = () => {
+  const { data, status } = useQuery("podcast", getPodCast, {
+    staleTime: 1000 * 60 * 60 * 24,
+    cacheTime: 1000 * 60 * 60 * 24,
+  });
 
+  const { searchText, onInputChange, onResetForm } = useForm({
+    searchText: "",
+  });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
-
+  const results = !searchText
+    ? data
+    : data.filter((podcast) =>
+        podcast.title.label.toLowerCase().includes(searchText.toLowerCase())
+      );
 
   return (
     <Container fluid>
-      <Grid.Container gap={2} justify="space-evenly" align="center">
-        <Row justify="flex-end">
-          <form action="" onSubmit={handleSubmit}>
-            <Input
-              type="text"
-              labelPlaceholder="Search"
-              name="query"
-              color="default"
-              // value={search}
-              // onChange={handleChange}
-              clearable
-              // onClearClick={clearSearch}
-            />
-          </form>
-        </Row>
-        {/*{searchText ? (
-          <>
-            <Row gap={2} align="center" justify="flex-start">
-              <Text>Results:</Text>
-            </Row>
-            <Row gap={2} align="center" justify="flex-start">
-              <Loading type="points-opacity" />
-            </Row>
-          </>
-        ) : null}
-        {producto.map((product) => {
-          return <ProductCard key={product.id} {...product} />;
-        })} */}
-      </Grid.Container>
+      <Row justify="flex-end">
+        <form action="">
+          <Input
+            type="text"
+            labelPlaceholder="Filter podcasts..."
+            name="searchText"
+            color="default"
+            value={searchText}
+            onChange={onInputChange}
+            clearable
+            onClearClick={onResetForm}
+          />
+        </form>
+      </Row>
+      <Row>
+        <Grid.Container gap={2} alignItems="center" justify="center">
+          {results?.map((product) => (
+            <PodCastCard key={product.id.attributes["im:id"]} {...product} />
+          ))}
+          {status === "loading" && <Loading />}
+        </Grid.Container>
+      </Row>
     </Container>
   );
 };
--- src/pages/Plp.jsx
+++ src/pages/Plp.jsx
@@ -1,29 +1,22 @@
-import { Container, Grid, Loading, Row, Spacer, Text } from "@nextui-org/react";
+import { Container, Grid, Row, Spacer, Text } from "@nextui-org/react";
 import HeaderComponent from "../components/HeaderComponent";
 import { Link } from "react-router-dom";
-import { getPodCast } from "../api/getPodCast";
 import { SearchComponent } from "../components/SearchComponent";
-import { useQuery } from "react-query";
 
 import "../styles/pages/plp.scss";
-import PodCastCard from "../components/PodCastCard";
 
 export const Plp = () => {
-  const { data, status } = useQuery("podcast", getPodCast, {
-    staleTime: 1000 * 60 * 60 * 24,
-    cacheTime: 1000 * 60 * 60 * 24,
-  });
-
   return (
-    <Container fluid>
+    <Container
+      fluid
+      css={{
+        maxWidth: "95%",
+      }}
+    >
       <HeaderComponent />
       <Spacer y={1} />
       <Grid.Container gap={2} justify="space-evenly" align="center">
-        <Row justify="flex-end">
-          0
-          <SearchComponent />
-        </Row>
-        <Row gap={2} align="center" justify="flex-start">
+        <Row align="center" justify="flex-start">
           <Link to={"/"}>
             <Text
               css={{
@@ -36,13 +29,9 @@ export const Plp = () => {
           </Link>
         </Row>
         <hr />
-
-        <Grid.Container gap={2} alignItems="center" justify="center">
-          {data?.map((product) => (
-            <PodCastCard key={product.id.attributes["im:id"]} {...product} />
-          ))}
-          {status === "loading" && <Loading />}
-        </Grid.Container>
+        <Row justify="center">
+          <SearchComponent />
+        </Row>
       </Grid.Container>
     </Container>
   );
